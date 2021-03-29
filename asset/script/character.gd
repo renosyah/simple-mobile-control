fc666e135eb9465101e46af89096e5b4efb944ef
renosyah <syahputrareno975@gmail.com>
2021-03-29 23:58:46 +0700

    add hp and following enemy

--- asset/script/character.gd
+++ asset/script/character.gd
@@ -1,5 +1,17 @@
 extends KinematicBody2D
 
+class_name Player
+
+signal hit_point_change(hp)
+
+onready var sprite = $sprite
+
+export var damage: = 5.0
+export var hit_point: = 100.0 setget _set_hit_point
+export var attack_distance: = 100.0 #80.0
+export var min_to_attack_distance: = 80.0 #100.0
+export var max_to_attack_distance: = 400.0
+
 const MOTION_SPEED = 500
 const TAN30DEG = tan(deg2rad(30))
 
@@ -20,24 +32,31 @@ func _physics_process(_delta):
 	motion.x = velocity.x
 	motion.y = velocity.y
 	
-	#motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	#motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
 	motion.y *= TAN30DEG
 	motion = motion.normalized() * MOTION_SPEED
 	
-	if motion.x > 0.0:
-		$sprite.scale.x = 1
-	else:
-		$sprite.scale.x = -1
-	
+	sprite.flip_h = motion.x > 0.0
+
 	if motion.x != 0.0 || motion.y !=  0.0:
-		$animation.play("walking")
+		$animation.play("character_walking")
 	else:
-		$animation.play("idle")
+		$animation.play("character_idle")
 	
 	move_and_slide(motion)
 
 func _on_touch_input_on_joystick_move(position):
 	velocity = position
+	
+func take_damage(damage):
+	self.hit_point -= damage
+	if self.hit_point <= 0:
+		queue_free()
+
+func _set_hit_point(hp):
+	hit_point = max(0,hp)
+	emit_signal("hit_point_change",hit_point)
+	
 
--- asset/script/touch_input.gd
+++ asset/script/touch_input.gd
@@ -1,7 +1,9 @@
 extends Node
 
+# broadcast to outside schene signal
 signal on_joystick_move(position)
 signal on_exit_button_press()
+
 
 func _ready():
 	pass
@@ -11,3 +13,7 @@ func _on_touchscreen_input_joystick(position):
 
 func _on_exit_button_pressed():
 	emit_signal("on_exit_button_press")
+
+func _on_character_hit_point_change(hp):
+	print(hp)
+	$v_container/h_top_ui/hit_point.value = hp
